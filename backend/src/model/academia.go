dd9e60c15b5dbe62843a629b22fc7da57cc0b559
lononeiro <lucassgs58@gmail.com>
2025-06-28 17:51:16 -0300

    update academia.go
    uptdated the "academia" model removing the comentarios

--- backend/src/model/academia.go
+++ backend/src/model/academia.go
@@ -1,10 +1,9 @@
 package model
 
 type Academia struct {
-	ID          int    `json:"id"`
-	Nome        string `json:"nome"`
-	Endereco    string `json:"endereco"`
-	Telefone    string `json:"telefone"`
-	Preco       string `json:"preco"`
-	Comentarios string `json:"comentarios"`
+	ID       int    `json:"id"`
+	Nome     string `json:"nome"`
+	Endereco string `json:"endereco"`
+	Telefone string `json:"telefone"`
+	Preco    string `json:"preco"`
 }
