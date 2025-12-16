87047113a8fc3cfc9a15f6471e8e57b73b60dddb
lononeiro <lucassgs58@gmail.com>
2025-12-16 08:10:18 -0300

    d

--- backend/src/model/academia.go
+++ backend/src/model/academia.go
@@ -1,13 +1,12 @@
 package model
 
 type Academia struct {
-	ID        uint     `json:"id"`
-	Nome      string   `json:"nome"`
-	Endereco  string   `json:"endereco"`
-	Telefone  string   `json:"telefone"`
-	Preco     string   `json:"preco"`
-	Descricao string   `json:"descricao"`
-	Imagens   []Imagem `json:"imagens" gorm:"foreignKey:AcademiaID"`
-	// Campo computado - não armazenado no banco, apenas para facilitar o frontend
-	ImagemPrincipal string `json:"imagem_principal" gorm:"-"`
+	ID              uint     `json:"id"`
+	Nome            string   `json:"nome"`
+	Endereco        string   `json:"endereco"`
+	Telefone        string   `json:"telefone"`
+	Preco           string   `json:"preco"`
+	Descricao       string   `json:"descricao"`
+	Imagens         []Imagem `json:"imagens" gorm:"foreignKey:AcademiaID"`
+	ImagemPrincipal string   `json:"imagem_principal" gorm:"-"`
 }
