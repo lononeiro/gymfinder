b1e669af6ddf7f889c8679d2bddd99f140a2ccb0
lononeiro <lucassgs58@gmail.com>
2025-12-16 15:40:32 -0300

    dsadsa

--- frontend/src/app/academia/[id]/page.tsx
+++ frontend/src/app/academia/[id]/page.tsx
@@ -60,8 +60,8 @@ export default function AcademiaDetalhePage() {
 
     setLoading(true);
     Promise.all([
-      fetch(`${API_URL}/academias/${id}`).then((r) => r.json()),
-      fetch(`${API_URL}/academias/${id}/comentarios`).then((r) => r.json()).catch(() => []),
+      fetch(`${API_URL}/academia/${id}`).then((r) => r.json()),
+      fetch(`${API_URL}/academia/${id}/comentarios`).then((r) => r.json()).catch(() => []),
     ])
       .then(([acadData, comms]) => {
         setAcademia(acadData);
